215a45e46ed27ace23e83aa154f1c85456f5d7e6
Sungho Kim <worstrocker@gmail.com>
2022-11-26 23:20:43 +0900

    update index.js

--- index.js
+++ index.js
@@ -17,58 +17,19 @@ function initialize() {
         navaidList[a].latitudeDecimal = util.convertMinutesToDecimal(navaidList[a].latitude);
         navaidList[a].longitudeDecimal = util.convertMinutesToDecimal(navaidList[a].longitude);
 
-        for (const aircraft in aircraftList) {
-            fileContent.push(
-                [
-                    aircraft,
-                    aircraftList[aircraft].aircraftType[0] + aircraftList[aircraft].wakeTurbulenceCategory[0] + aircraftList[aircraft].engineCount + aircraftList[aircraft].engineType[0],
-                    aircraftList[aircraft].manufacturer,
-                    aircraftList[aircraft].description
-                ].join("\t"))
+        // minutes to decimal conversion
+        for (var a in airportList) {
+            airportList[a].latitudeDecimal = util.convertMinutesToDecimal(airportList[a].latitude);
+            airportList[a].longitudeDecimal = util.convertMinutesToDecimal(airportList[a].longitude);
         }
 
-        fs.writeFileSync('euroscope/ICAO_aircraft.txt', fileContent.join("\n"));
-    },
-    createEuroscopeAirlineFile() {
-        let fileContent = [];
-
-        for (const airline in airlineList) {
-            fileContent.push(
-                [
-                    airline,
-                    airlineList[airline],
-                    airlineList[airline]
-                ].join("\t"))
-        }
-
-        fs.writeFileSync('euroscope/ICAO_airline.txt', fileContent.join("\n"));
-    },
-    createEuroscopeAirportFile() {
-        let fileContent = [];
-
-        for (const airport in airportList) {
-            fileContent.push(
-                [
-                    airportList[airport].name,
-                    airportList[airport].description,
-                    airportList[airport].country
-                ].join("\t"))
-        }
-
-        fs.writeFileSync('euroscope/ICAO_airport.txt', fileContent.join("\n"));
+        // console.log(JSON.stringify(navaidList, null, '\t'));
     }
-
-    // minutes to decimal conversion
-    for (var a in airportList) {
-        airportList[a].latitudeDecimal = util.convertMinutesToDecimal(airportList[a].latitude);
-        airportList[a].longitudeDecimal = util.convertMinutesToDecimal(airportList[a].longitude);
-    }
-
-    // console.log(JSON.stringify(navaidList, null, '\t'));
 }
 
 // ----------------- run -------------------
 initialize();
+sector.generateSectorFile();
 euroscope.createEuroscopeAircraftFile();
 euroscope.createEuroscopeAirlineFile();
 euroscope.createEuroscopeAirportFile();
