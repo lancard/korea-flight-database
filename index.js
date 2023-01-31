32fc90b78d83c685f648435d1bd966d4fe40d5b9
Sungho Kim <worstrocker@gmail.com>
2023-01-31 13:28:32 +0900

    adjust sorting (sid/star/approach)

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ Array.prototype.last = function () {
 }
 
 function initialize() {
+    // sort procedureList
+    procedureList.sort((a, b) => {
+        if (a.name < b.name)
+            return -1;
+        if (a.name > b.name)
+            return 1;
+
+        return 0;
+    });
+
     // check all fix in navaids
     procedureList.forEach(p => {
         p.fixList.forEach(e => {
